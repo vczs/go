bf2a8e217a51cd0504bf307b0f46dec657c5d46d
vczs <hi.vincz@gmail.com>
2022-02-23 22:29:54 +0800

    管道update

--- 管道/channel入门.go
+++ 管道/channel入门.go
@@ -4,30 +4,31 @@ import "fmt"
 
 /**
 管道(channel)：的本质就是一个数据结构 - 单向队列。
+
 特点：
 1.先进先出（FIFO:first in first out）
 2.线程安全，多goroutine访问时，不会发生资源竞争问题，无需加锁，就是说channel本身就是线程安全的。
-3.channel是引用类型，必须初始化才能完成数据写入，即make后才能使用。
-4.channel是有数据类型的，一个string数据类型的channel只能存放string类型数据。
-5.channel没有扩容机制，当长度等于容量时再放入数据会报错，长度不能超过容量。
-6.在没有使用协程的情况下，如果管道内的数据全部取出（长度等于零），再取就会报错。
-7.当管道放满时，再放就会报错；但将管道内数据取出后，仍可以继续放入，取出几个数据便可以再放入几个数据，直至再次放满。
-8.如果编译器发现一个管道只有放入没有取出，那么该管道就会阻塞，编译器报错；管道存取频率不一致，无所谓，编译器不会报错。
+
+注意事项：
+1.channel是指针类型，必须初始化才能完成数据写入，即必须make后才能使用。
+2.channel是有数据类型的，一个string数据类型的channel只能存放string类型数据。
+3.make时不设置容量就是无缓冲channel，设置大于0的容量就是有缓冲channel。
+4.channel关闭后不能再放数据，再放会报错。
+
+在没有使用协程的情况下：
+1.当channel内放满时再放就会报错，长度不能超过容量；当channel内的数据全部取出时(长度等于零)再取就会报错。
+2.当channel内放满时再放就会报错，但将管道内数据取出后，仍可以继续放入；取出几个数据便可以再放入几个数据，直至再次放满。
+3.使用for range对未关闭的管道进行遍历会报错。
 
 channel声明:
-var 变量名 chan 数据类型
-var intChan chan int （intChan用于存放int数据）
-var mapChan chan map[int]string （mapChan用于存放int数据）
-var peopleChan_1 chan People（结构体）
-var peopleChan_2 chan *People（结构体指针）
-管道必须make后才能使用：make(chan 数据类型 , 容量)
+var ch chan 数据类型
+管道可以声明为只读或只写（管道在默认情况下是双向的）
+var ch chan<- 数据类型 (只写管道)（管道只能放入数据，不能取出）
+var ch <-chan 数据类型 (只读管道)（管道只能取出数据，不能放入）
+ch = make(chan 数据类型 , 容量)		// 管道必须make后才能使用
+*/
 
-管道可以声明为只读或只写（管道在默认情况下是双向的）：
-var intChan chan<- int (只写管道)（管道只能放入数据，不能取出）
-var intChan <-chan int (只读管道)（管道只能取出数据，不能放入）
- */
-
-func main(){
+func main() {
 	case_1()
 	case_2()
 }
@@ -35,32 +36,31 @@ func main(){
 /**
 channel声明和数据存取
 */
-func case_1(){
+func case_1() {
 	//管道声明和实例化
-	var intChan chan int = make(chan int , 3) //intChan必须make后才能使用，chan int是intChan的数据类型，3是intChan的容量
+	var intChan chan int = make(chan int, 3) //intChan必须make后才能使用，chan int是intChan的数据类型，3是intChan的容量
 	//向管道内放入数据
-	intChan <- 10 //将10放入管道
-	intChan <- 12 //将12放入管道
-	fmt.Printf("intChan的长度：%v  intChan的容量：%v\n",len(intChan),cap(intChan))//查看管道的长度和容量
+	intChan <- 10                                                            //将10放入管道
+	intChan <- 12                                                            //将12放入管道
+	fmt.Printf("intChan的长度：%v  intChan的容量：%v\n", len(intChan), cap(intChan)) //查看管道的长度和容量
 	//从管道内取出数据
 	var num1 int
-	num1 = <- intChan
-	num2 := <- intChan
-	fmt.Printf("num1=%v   num2=%v\n",num1,num2)
-	fmt.Printf("intChan的长度：%v  intChan的容量：%v\n",len(intChan),cap(intChan))//查看管道的长度和容量
+	num1 = <-intChan
+	num2 := <-intChan
+	fmt.Printf("num1=%v   num2=%v\n", num1, num2)
+	fmt.Printf("intChan的长度：%v  intChan的容量：%v\n", len(intChan), cap(intChan)) //查看管道的长度和容量
 }
 
-
-func case_2(){
+func case_2() {
 	//管道声明和实例化
-	var intChan chan int = make(chan int , 2)
+	var intChan chan int = make(chan int, 2)
 	//向管道内放入数据
-	intChan <- 25 //将25放入管道
-	intChan <- 15 //将15放入管道
-	fmt.Printf("intChan的长度：%v  intChan的容量：%v\n",len(intChan),cap(intChan))//查看管道的长度和容量
+	intChan <- 25                                                            //将25放入管道
+	intChan <- 15                                                            //将15放入管道
+	fmt.Printf("intChan的长度：%v  intChan的容量：%v\n", len(intChan), cap(intChan)) //查看管道的长度和容量
 	//当管道放满时，再放就会报错。
-	<- intChan //将intChan管道内的数据取出来不要了，给管道清理个位置来放入需要使用的数据
-	fmt.Printf("intChan的长度：%v  intChan的容量：%v\n",len(intChan),cap(intChan))//查看管道的长度和容量
-	intChan <- 18 	//但将管道内数据取出后，仍可以继续放入，取出几个数据便可以再放入几个数据，直至再次放满。
-	fmt.Printf("intChan的长度：%v  intChan的容量：%v\n",len(intChan),cap(intChan))//查看管道的长度和容量
+	<-intChan                                                                //将intChan管道内的数据取出来不要了，给管道清理个位置来放入需要使用的数据
+	fmt.Printf("intChan的长度：%v  intChan的容量：%v\n", len(intChan), cap(intChan)) //查看管道的长度和容量
+	intChan <- 18                                                            //但将管道内数据取出后，仍可以继续放入，取出几个数据便可以再放入几个数据，直至再次放满。
+	fmt.Printf("intChan的长度：%v  intChan的容量：%v\n", len(intChan), cap(intChan)) //查看管道的长度和容量
 }
--- 管道/select读取管道.go
+++ 管道/select读取管道.go
@@ -2,27 +2,30 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	case_8()
 }
 
 /**
 传统方法在遍历管道时，如果管道没有关闭就会报错。
-可以用select方法可以在管道未关闭的情况下遍历管道。
- */
-func case_8(){
-	intChan := make(chan int,5)
+select方法可以在管道未关闭的情况下遍历管道。
+*/
+func case_8() {
+	intChan := make(chan int, 5)
 	intChan <- 20
 	intChan <- 30
 	intChan <- 40
-	A:for { //使用for循环一直执行select
-		select{
-			//如果intChan没有关闭，当这里读取不到数据就会进入下一个case不会报错
-			case v := <-intChan:
-				fmt.Printf("读取到：%v\n",v)
-			default:
-				fmt.Printf("读取不到数据，循环结束！！！")
-				break A
+A:
+	for { // 使用for循环一直执行select
+		select {
+		// 如果intChan没有关闭，当这里读取不到数据就会进入下一个case不会报错
+		case v := <-intChan:
+			fmt.Printf("读取到：%v\n", v)
+		// 加default的select是没有阻塞的，如果没有满足的case就执行default语句；不会阻塞等待满足case条件
+		// 如果不加default则select有阻塞，如果没有满足的case就阻塞等待，只有当满足的case管道读写操作时才会结束阻塞
+		default:
+			fmt.Printf("读取不到数据，循环结束！！！")
+			break A
 		}
 	}
 }
