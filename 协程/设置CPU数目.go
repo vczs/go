9d27402306e6da6b088e757070c43e207d2ddad8
vczs <hi.vincz@gmail.com>
2022-03-01 07:30:33 +0800

    协程

--- 协程/设置CPU数目.go
+++ 协程/设置CPU数目.go
@@ -5,10 +5,10 @@ import (
 	"runtime"
 )
 
-func main(){
-	cpuNum := runtime.NumCPU() //获取当前设备CPU数目
-	fmt.Printf("cpuNum = %v\n数据类型：%T\n",cpuNum,cpuNum)
+func main() {
+	cpuNum := runtime.NumCPU() //获取当前设备CPU核数目
+	fmt.Printf("cpuNum = %v\n数据类型：%T\n", cpuNum, cpuNum)
 
-	runtime.GOMAXPROCS(4)//设置当前程序使用多少个CPU
+	runtime.GOMAXPROCS(4) //设置当前程序使用多少个CPU核数目
 	fmt.Println("设置成功。")
 }
